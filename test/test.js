5a8fcd32704a2ddd865ed96e945ebd93f45f4d38
Ricardo Roman <r.rmn92@gmail.com>
2017-01-12 05:58:22 +0000

    First commit

---
+++ test/test.js
@@ -0,0 +1,120 @@
+var assert = require('assert');
+var evangelizo = require('../lib/evangelizo.js');
+
+var testDate = new Date(new Date().getFullYear(), new Date().getMonth(), 1);
+
+describe('Evangelizo', function() {
+        describe('#getSaint() without parameters', function() {
+                it('should return a valid string', function(done) {
+
+                        evangelizo.getSaint().then((str) => {
+                            if (typeof str === 'string') { done(); }
+                            else done(err);
+                        }, (err) => {
+                            done(err);
+                        });
+
+                });
+        });
+
+        describe('#getSaint() with date and lang parameters', function() {
+            it('should return a valid string', function(done) {
+
+                evangelizo.getSaint({ lang: 'SP', date: testDate }).then((str) => {
+                    if (typeof str === 'string') done();
+                    else done(err);
+                }, (err) => {
+                    done(err);
+                });
+
+            });
+        });
+
+        describe('#getfeast() without parameters', function() {
+            it('should return a valid string', function(done) {
+
+                evangelizo.getFeast().then((str) => {
+                    if (typeof str === 'string') done();
+                    else done(err);
+                }, (err) => {
+                    done(err);
+                });
+
+            });
+        });
+
+        describe('#getFeast() with date and lang parameters', function() {
+            it('should return a valid string', function(done) {
+
+                evangelizo.getFeast({ lang: 'SP', date: testDate }).then((str) => {
+                    if (typeof str === 'string') done();
+                    else done(err);
+                }, (err) => {
+                    done(err);
+                });
+
+            });
+        });
+
+        describe('#getReading() only with content parameter', function() {
+            it('should return a valid string', function(done) {
+
+                evangelizo.getReading('FR').then((str) => {
+                    if (typeof str === 'undefined') done();
+                    else done(err);
+                }, (err) => {
+                    done(err);
+                });
+
+            });
+        });
+
+        describe('#getReading() with content, date and lang parameters', function() {
+            it('should return a valid string', function(done) {
+
+                evangelizo.getReading('FR', { date: testDate, lang: 'SP' }).then((str) => {
+                    if (typeof str === 'undefined') done();
+                    else done(err);
+                }, (err) => {
+                    done(err);
+                });
+
+            });
+        });
+
+
+});
+
+
+
+
+
+evangelizo.getReadingLt('FR').then((str) => {
+        if (typeof str === 'undefined') done();
+        else done(err);
+}, (err) => {
+        done(err);
+});
+
+evangelizo.getReadingSt('FR').then((str) => {
+        if (typeof str === 'undefined') done();
+        else done(err);
+}, (err) => {
+        done(err);
+});
+
+
+
+evangelizo.getReadingLt('FR', { date: testDate, lang: 'SP' }).then((str) => {
+        if (typeof str === 'undefined') done();
+        else done(err);
+}, (err) => {
+        done(err);
+});
+
+evangelizo.getReadingSt('FR', { date: testDate, lang: 'SP' }).then((str) => {
+        if (typeof str === 'undefined') done();
+        else done(err);
+}, (err) => {
+        done(err);
+});
